a4a966bd44aee83e3b5a8e70b0c2749ef0d59abd
withon <withon.wei@gmail.com>
2024-10-09 00:00:44 +0800

    feat:新增桃幽选择条

--- pages/taoyou.vue
+++ pages/taoyou.vue
@@ -40,6 +40,260 @@
         </div>
       </div>
     </div>
+    <div class="swiper-scroll" :style="{ justifyContent: swiperScrollJustifyContent }">
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2001</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part swiper-scroll-part-active">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2002</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2003</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2004</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2005</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2006</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2007</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2008</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2009</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2010</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2011</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2012</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2013</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2014</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2015</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2016</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2017</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2018</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2019</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2020</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2021</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2022</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2023</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2024</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2025</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2026</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2026</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2028</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2029</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2030</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2031</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2032</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2033</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2034</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2035</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2036</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2037</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2038</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2039</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2040</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2041</span>
+        </a>
+      </div>
+      <div class="swiper-scroll-part">
+        <a href="javascript:void(0)">
+          <img src="/images/member/yueshou.jpeg" alt="Picture" />
+          <span>2042</span>
+        </a>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -51,15 +305,22 @@ const swiperBg01 = computed(() => {
     : "url('/images/taoyou/bg_s_01.jpg')";
 });
 
+const swiperScrollJustifyContent = ref('center');
+
 /**
  * 监控宽度改变高度
  */
 const widthListenerMountedFn = () => {
-  windowWidth.value = document.documentElement.clientWidth;
   // 监听窗口大小变化
   const handleResize = () => {
     windowWidth.value = document.documentElement.clientWidth;
+    let swiperScroll = document.querySelector('div .swiper-scroll');
+    if (swiperScroll) {
+      swiperScrollJustifyContent.value =
+        swiperScroll.scrollWidth > swiperScroll.clientWidth ? 'flex-start' : 'center';
+    }
   };
+  handleResize();
   window.addEventListener('resize', handleResize);
   // 在组件卸载时移除事件监听器
   onUnmounted(() => {
@@ -75,6 +336,128 @@ onMounted(() => {
 </script>
 
 <style lang="scss" scoped>
+.swiper {
+  position: relative;
+}
+
+.swiper-scroll {
+  position: absolute;
+  display: -webkit-flex; /* Safari */
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  flex-wrap: nowrap;
+  width: 100%;
+  overflow-x: auto;
+  overflow-y: hidden;
+  /* 自定义滚动条的整体样式 */
+  &::-webkit-scrollbar {
+    width: 8px; /* 设置滚动条的宽度 */
+    height: 8px; /* 设置滚动条的高度 */
+    background-color: transparent; /* 设置滚动条的背景为透明 */
+  }
+
+  /* 自定义滚动轨迹的样式 */
+  &::-webkit-scrollbar-track {
+    background-color: transparent; /* 设置滚动轨迹的背景为透明 */
+  }
+
+  /* 自定义滚动滑块的样式 */
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 170, 255, 0.5); /* 设置滚动滑块为灰色半透明 */
+    border-radius: 6px; /* 设置滚动滑块的圆角 */
+    visibility: visible; /* 默认隐藏滚动滑块 */
+  }
+
+  /* 自定义鼠标悬浮时滚动滑块的样式 */
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: rgba(0, 170, 255, 0.8); /* 设置滚动滑块为灰色半透明 */
+    visibility: visible; /* 鼠标悬浮时显示滚动滑块 */
+  }
+
+  /* 设置滚动条滑块在滚动时的样式 */
+  &::-webkit-scrollbar-thumb:active {
+    background-color: rgb(0, 170, 255); /* 滚动时的滑块颜色 */
+  }
+
+  /* 隐藏滚动条两侧的箭头 */
+  &::-webkit-scrollbar-button {
+    display: none; /* 隐藏滚动条两侧的箭头 */
+  }
+  @media screen and (max-width: breakpoint-max(md)) {
+    top: 56px;
+    height: 100px;
+    background: rgba(255, 255, 255, 0.35);
+  }
+  @media (min-width: breakpoint-min(md)) and (max-width: breakpoint-max(lg)) {
+    top: 56px;
+    height: 100px;
+    background: linear-gradient(to bottom, rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0.75));
+  }
+  @media (min-width: breakpoint-min(lg)) {
+    bottom: 0;
+    height: 150px;
+    padding: 10px;
+    background: linear-gradient(to bottom, rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0.75));
+  }
+}
+
+.swiper-scroll-part {
+  -webkit-box-sizing: content-box;
+  -moz-box-sizing: content-box;
+  box-sizing: content-box;
+  flex: 0 0 auto;
+  position: relative;
+  margin-left: 5px;
+  margin-right: 5px;
+  border-radius: 15px;
+  background-color: white;
+  @media (max-width: breakpoint-max(lg)) {
+    padding: 4px;
+    height: 65px;
+    width: 65px;
+  }
+  @media (min-width: breakpoint-min(lg)) {
+    padding: 5px;
+    height: 90px;
+    width: 90px;
+  }
+
+  img {
+    height: 100%;
+    width: 100%;
+    border-radius: 10px;
+    object-fit: cover;
+  }
+
+  span {
+    display: inline-block;
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: 0 15px;
+    background: #000;
+    color: #fff;
+    font-weight: 700;
+    text-align: center;
+    font-size: 1rem;
+    border-radius: 5px;
+    @media (max-width: breakpoint-max(lg)) {
+      bottom: -5px;
+      font-size: 0.8rem;
+    }
+    @media (min-width: breakpoint-min(lg)) {
+      bottom: -12px;
+      font-size: 1rem;
+    }
+  }
+}
+
+.swiper-scroll-part-active {
+  border: 4px rgb(0, 170, 255) solid;
+}
+
 .swiper-bg-color {
   height: 100%;
   margin: 0 auto;
@@ -112,12 +495,12 @@ onMounted(() => {
   text-align: center;
   color: #000;
   border-radius: 15px;
-  z-index: 1;
   padding: 30px;
   overflow: auto;
   /* 自定义滚动条的整体样式 */
   &::-webkit-scrollbar {
     width: 8px; /* 设置滚动条的宽度 */
+    height: 8px; /* 设置滚动条的高度 */
     background-color: transparent; /* 设置滚动条的背景为透明 */
   }
 
